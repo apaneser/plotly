c47f4df72b59efc706c4eb0ea93a6151a6f7cd19
apaneser <56094764+apaneser@users.noreply.github.com>
2021-09-12 01:10:32 -0700

    deliverable 3

--- charts.js
+++ charts.js
@@ -125,6 +125,42 @@ function buildCharts(sample) {
     };
 
     // 3. Use Plotly to plot the data with the layout.
-    Plotly.newPlot('bubble', bubbleData); 
+    Plotly.newPlot('bubble', bubbleData, bubbleLayout); 
+
+
+    // deliverable 3
+    // 3. Create a variable that holds the washing frequency.
+    var metadata = data.metadata;
+    console.log(metadata);
+    var wfreq = metadata.filter(item => item.id == sample)[0].wfreq;
+    console.log(wfreq);
+
+    // 4. Create the trace for the gauge chart.
+    var gaugeData = [{
+      domain: {x: [0,1], y: [0,1]},
+      value: wfreq,
+      title: {text: "Scrubs per Week"},
+      type: "indicator",
+      mode: "gauge+number",
+      gauge: {
+        axis: {range: [null, 10]},
+        bar: {color: "black"},
+        steps: [
+          {range: [0,2], color: 'red'},
+          {range: [2,4], color: 'orange'},
+          {range: [4,6], color: 'yellow'},
+          {range: [6,8], color: 'lightgreen'},
+          {range: [8,10], color: 'green'},
+        ]
+      }
+    }];
+    
+    // 5. Create the layout for the gauge chart.
+    var gaugeLayout = { 
+     title: "Belly Button Washing Frequency"
+    };
+
+    // 6. Use Plotly to plot the gauge data and layout.
+    Plotly.newPlot('gauge', gaugeData, gaugeLayout);
   });
 }
